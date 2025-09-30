2a80d53b6fae41e643c9ffeffdd384c3e529e75a
Ayush <73355805+ayushb2005@users.noreply.github.com>
2025-09-30 08:57:31 -0700

    Revert "mobileTest"

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,21 +7,19 @@ const Footer = ()  => {
   return (
     <div>
         <br/>
-        <div className='flex flex-nowrap sm:flex-wrap items-center gap-3 sm:gap-4 max-w-md text-left mx-auto text-[11px] sm:text-xs px-4'>
-            <footer className='whitespace-nowrap shrink-0'>© 2025</footer>
-            <footer className='whitespace-nowrap shrink-0'>Ayush Bhanushali</footer>
-            <a href='mailto:ayushb2005@gmail.com' className='flex items-center gap-2 whitespace-nowrap shrink-0'>
-                <footer>ayushb2005@gmail.com</footer>
-                <img className='w-5 h-5 sm:w-5 sm:h-5' src={email} alt='Ayush Email'/>
+        <div className='flex gap-4 max-w-md text-left mx-auto font-style: italic text-xs'>
+            <footer>© 2025</footer>
+            <footer>Ayush Bhanushali</footer>
+            <footer>ayushb2005@gmail.com</footer>
+            <a href="https://github.com/ayushb2005">
+                <img className= 'w-5 h-5' src={github} alt='Github'/>
             </a>
-            <div className='flex items-center gap-3 sm:gap-4 shrink-0'>
-                <a href="https://github.com/ayushb2005">
-                    <img className='w-5 h-5' src={github} alt='Github'/>
-                </a>
-                <a href='https://www.linkedin.com/in/ayushbhanushali/'>
-                    <img className='w-5 h-5' src={linkedin} alt='LinkedIn'/>
-                </a>
-            </div>
+            <a href='https://www.linkedin.com/in/ayushbhanushali/'>
+                <img className='w-5 h-5' src={linkedin} alt='LinkedIn'/>
+            </a>
+            <a href='mailto:ayushb2005@gmail.com'>
+                <img className='w-5 h-5' src={email} alt='Ayush Email'/>
+            </a>
         </div>
     </div>
   )
